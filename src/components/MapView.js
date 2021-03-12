6cb307fa5081d25429dddd36ace5afdf75e165ec
Sergio Ríos <sergio.rios@avaibook.com>
2021-03-12 11:48:48 +0100

    Add new AvaiBook market

--- src/components/MapView.js
+++ src/components/MapView.js
@@ -1,7 +1,8 @@
 import React from 'react'
-
-import { MapContainer, TileLayer } from 'react-leaflet'
+import { MapContainer, TileLayer, Popup } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css'
+
+import AvaiBookMarker from './AvaiBookMarker'
 
 const MapView = () => {
   return (
@@ -10,6 +11,10 @@ const MapView = () => {
         url="http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}"
         subdomains={['mt0','mt1','mt2','mt3']}
         attribution='<a href="https://www.google.es/maps/preview">Google Maps</a>'
+      />
+      <AvaiBookMarker
+        size={[50, 50]}
+        position={[37.2759242, -7.0016856]}
       />
     </MapContainer>
   )
