2e8d814b25268a96c02086be2a9980ac31017b73
Sergio Ríos <sergio.rios@avaibook.com>
2021-03-12 10:40:25 +0100

    Change TyleLayer to Google Maps

--- src/components/MapView.js
+++ src/components/MapView.js
@@ -7,8 +7,9 @@ const MapView = () => {
   return (
     <MapContainer center={{ lat: '37.276418', lng: '-7.001000' }} zoom={16}>
       <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        url="http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}"
+        subdomains={['mt0','mt1','mt2','mt3']}
+        attribution='<a href="https://www.google.es/maps/preview">Google Maps</a>'
       />
     </MapContainer>
   )
