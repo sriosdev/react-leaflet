7d00914da9e02da542d106b1707bb344ba0fb43f
Sergio Ríos <sergio.rios@avaibook.com>
2021-03-26 13:25:41 +0100

    Getting competitor inside circle area

--- src/components/MapView.js
+++ src/components/MapView.js
@@ -1,28 +1,19 @@
 import React from 'react'
-import { MapContainer, TileLayer, FeatureGroup, Circle, Marker } from 'react-leaflet'
-import Leaflet from 'leaflet'
+import { MapContainer, TileLayer, FeatureGroup } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css'
-import AvaiBookPopup from './AvaiBookPopup.js'
-import AvaiBookMarker from './AvaiBookMarker.js'
-
+import CompetitorsArea from './CompetitorsArea'
 
 const MapView = () => {
-  const randomCoord = [37.276418, -7.001000]
-
+  console.log(process.env.REACT_APP_AIRDNA_URL)
   return (
-    <MapContainer center={{ lat: '37.276418', lng: '-7.001000' }} zoom={16}>
+    <MapContainer center={{ lat: '37.276418', lng: '-7.001000' }} zoom={13}>
       <TileLayer
         url="http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}"
         subdomains={['mt0','mt1','mt2','mt3']}
         attribution='<a href="https://www.google.es/maps/preview">Google Maps</a>'
       />
       <FeatureGroup>
-        <Circle center={randomCoord} radius={200} />
-        <AvaiBookMarker position={randomCoord} size={[50, 50]}>
-          <AvaiBookPopup>
-            <p>Hello <strong>Sergio</strong><br></br>Where do you live?</p>
-          </AvaiBookPopup>
-        </AvaiBookMarker>
+        <CompetitorsArea />
       </FeatureGroup>
     </MapContainer>
   )
